1d4972e2801231fd8ee53fd931277eec6194a1c2
unknown <patil.ratnesh1997@gmail.com>
2023-07-21 22:38:51 +0530

    day 1

---
+++ Snake/src/GameFrame.java
@@ -0,0 +1,7 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+	GameFrame(){
+		
+	}
+}
---
+++ Snake/src/GamePanel.java
@@ -0,0 +1,15 @@
+import javax.swing.JPanel;
+import java.awt.event.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+	GamePanel(){
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+	
